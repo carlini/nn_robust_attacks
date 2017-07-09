1193c79866482d067d15dbdab61a3b50a6903834
Nicholas Carlini <nicholas@carlini.com>
2017-07-09 15:02:15 -0700

    Update mnist model to Keras v2

--- setup_mnist.py
+++ setup_mnist.py
@@ -14,7 +14,7 @@ import urllib.request
 
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Activation, Flatten
-from keras.layers import Convolution2D, MaxPooling2D
+from keras.layers import Conv2D, MaxPooling2D
 from keras.utils import np_utils
 from keras.models import load_model
 
@@ -67,16 +67,16 @@ class MNISTModel:
 
         model = Sequential()
 
-        model.add(Convolution2D(32, 3, 3,
-                                input_shape=(28, 28, 1)))
+        model.add(Conv2D(32, (3, 3),
+                         input_shape=(28, 28, 1)))
         model.add(Activation('relu'))
-        model.add(Convolution2D(32, 3, 3))
+        model.add(Conv2D(32, (3, 3)))
         model.add(Activation('relu'))
         model.add(MaxPooling2D(pool_size=(2, 2)))
         
-        model.add(Convolution2D(64, 3, 3))
+        model.add(Conv2D(64, (3, 3)))
         model.add(Activation('relu'))
-        model.add(Convolution2D(64, 3, 3))
+        model.add(Conv2D(64, (3, 3)))
         model.add(Activation('relu'))
         model.add(MaxPooling2D(pool_size=(2, 2)))
         
