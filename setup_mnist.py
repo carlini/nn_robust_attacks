363b90447c2361df279977aa02cbb6d812f32b0c
Anshuman Suri <anshuman14021@iiitd.ac.in>
2017-10-04 10:21:06 +0530

    Switching to loading data from keras

--- setup_mnist.py
+++ setup_mnist.py
@@ -7,10 +7,6 @@
 
 import tensorflow as tf
 import numpy as np
-import os
-import pickle
-import gzip
-import urllib.request
 
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Activation, Flatten
@@ -18,46 +14,25 @@ from keras.layers import Conv2D, MaxPooling2D
 from keras.utils import np_utils
 from keras.models import load_model
 
-def extract_data(filename, num_images):
-    with gzip.open(filename) as bytestream:
-        bytestream.read(16)
-        buf = bytestream.read(num_images*28*28)
-        data = np.frombuffer(buf, dtype=np.uint8).astype(np.float32)
-        data = (data / 255) - 0.5
-        data = data.reshape(num_images, 28, 28, 1)
-        return data
+from keras.datasets import mnist
+from keras.utils import np_utils
 
-def extract_labels(filename, num_images):
-    with gzip.open(filename) as bytestream:
-        bytestream.read(8)
-        buf = bytestream.read(1 * num_images)
-        labels = np.frombuffer(buf, dtype=np.uint8)
-    return (np.arange(10) == labels[:, None]).astype(np.float32)
 
 class MNIST:
     def __init__(self):
-        if not os.path.exists("data"):
-            os.mkdir("data")
-            files = ["train-images-idx3-ubyte.gz",
-                     "t10k-images-idx3-ubyte.gz",
-                     "train-labels-idx1-ubyte.gz",
-                     "t10k-labels-idx1-ubyte.gz"]
-            for name in files:
-
-                urllib.request.urlretrieve('http://yann.lecun.com/exdb/mnist/' + name, "data/"+name)
-
-        train_data = extract_data("data/train-images-idx3-ubyte.gz", 60000)
-        train_labels = extract_labels("data/train-labels-idx1-ubyte.gz", 60000)
-        self.test_data = extract_data("data/t10k-images-idx3-ubyte.gz", 10000)
-        self.test_labels = extract_labels("data/t10k-labels-idx1-ubyte.gz", 10000)
-        
-        VALIDATION_SIZE = 5000
-        
-        self.validation_data = train_data[:VALIDATION_SIZE, :, :, :]
-        self.validation_labels = train_labels[:VALIDATION_SIZE]
-        self.train_data = train_data[VALIDATION_SIZE:, :, :, :]
-        self.train_labels = train_labels[VALIDATION_SIZE:]
-
+	(X_train, y_train), (X_test, y_test) = mnist.load_data()
+	X_train = X_train.reshape(X_train.shape[0], 1, img_rows, img_cols)
+	X_test = X_test.reshape(X_test.shape[0], 1, img_rows, img_cols)
+	X_train = X_train.astype('float32')
+	X_test = X_test.astype('float32')
+	X_train /= 255
+	X_test /= 255
+	Y_train = np_utils.to_categorical(y_train, nb_classes)
+	Y_test = np_utils.to_categorical(y_test, nb_classes)
+	self.train_data = X_train
+	self.train_labels = Y_train
+	self.test_data = X_test
+	self.test_labels = Y_test
 
 class MNISTModel:
     def __init__(self, restore, session=None):
@@ -73,13 +48,13 @@ class MNISTModel:
         model.add(Conv2D(32, (3, 3)))
         model.add(Activation('relu'))
         model.add(MaxPooling2D(pool_size=(2, 2)))
-        
+
         model.add(Conv2D(64, (3, 3)))
         model.add(Activation('relu'))
         model.add(Conv2D(64, (3, 3)))
         model.add(Activation('relu'))
         model.add(MaxPooling2D(pool_size=(2, 2)))
-        
+
         model.add(Flatten())
         model.add(Dense(200))
         model.add(Activation('relu'))
--- train_models.py
+++ train_models.py
@@ -5,8 +5,9 @@
 ## This program is licenced under the BSD 2-Clause licence,
 ## contained in the LICENCE file in this directory.
 
-
+import keras
 import numpy as np
+
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Activation, Flatten
 from keras.layers import Conv2D, MaxPooling2D
@@ -17,14 +18,24 @@ from setup_mnist import MNIST
 from setup_cifar import CIFAR
 import os
 
-def train(data, file_name, params, num_epochs=50, batch_size=128, train_temp=1, init=None):
+# Don't hog GPU
+config = tf.ConfigProto()
+config.gpu_options.allow_growth=True
+sess = tf.Session(config=config)
+keras.backend.set_session(sess)
+
+if keras.backend.image_dim_ordering() != 'th':
+		keras.backend.set_image_dim_ordering('th')
+
+
+def train(data, file_name, params, num_epochs=50, batch_size=16, train_temp=1, init=None):
     """
     Standard neural network training procedure.
     """
     model = Sequential()
 
     print(data.train_data.shape)
-    
+
     model.add(Conv2D(params[0], (3, 3),
                             input_shape=data.train_data.shape[1:]))
     model.add(Activation('relu'))
@@ -45,7 +56,7 @@ def train(data, file_name, params, num_epochs=50, batch_size=128, train_temp=1, 
     model.add(Dense(params[5]))
     model.add(Activation('relu'))
     model.add(Dense(10))
-    
+
     if init != None:
         model.load_weights(init)
 
@@ -54,24 +65,23 @@ def train(data, file_name, params, num_epochs=50, batch_size=128, train_temp=1, 
                                                        logits=predicted/train_temp)
 
     sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
-    
+
     model.compile(loss=fn,
                   optimizer=sgd,
                   metrics=['accuracy'])
-    
+
     model.fit(data.train_data, data.train_labels,
               batch_size=batch_size,
-              validation_data=(data.validation_data, data.validation_labels),
-              nb_epoch=num_epochs,
+              validation_split=0.2,
+              epochs=num_epochs,
               shuffle=True)
-    
 
     if file_name != None:
         model.save(file_name)
 
     return model
 
-def train_distillation(data, file_name, params, num_epochs=50, batch_size=128, train_temp=1):
+def train_distillation(data, file_name, params, num_epochs=50, batch_size=16, train_temp=1):
     """
     Train a network using defensive distillation.
 
@@ -82,7 +92,7 @@ def train_distillation(data, file_name, params, num_epochs=50, batch_size=128, t
     if not os.path.exists(file_name+"_init"):
         # Train for one epoch to get a good starting point.
         train(data, file_name+"_init", params, 1, batch_size)
-    
+
     # now train the teacher at the given temperature
     teacher = train(data, file_name+"_teacher", params, num_epochs, batch_size, train_temp,
                     init=file_name+"_init")
@@ -102,7 +112,7 @@ def train_distillation(data, file_name, params, num_epochs=50, batch_size=128, t
     predicted = student.predict(data.train_data)
 
     print(predicted)
-    
+
 if not os.path.isdir('models'):
     os.makedirs('models')
 
